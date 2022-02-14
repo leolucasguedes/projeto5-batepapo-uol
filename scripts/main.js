a73d4afc58f81d90395464a28d010a25cb52bf38
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-02-14 16:08:32 -0300

    Nome do usuário enviado ao servidor

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,43 @@
+let chat = document.querySelector("#chat");
+let logged = false;
+
+let nomeUsuario;
+let participants = [];
+let messages = [];
+
+let startMessageCount;
+let lastMessageLoop = null;
+let lastMessageArray = null;
+
+
+//usuario
 function pedirNomeUsuario(){
-    let nomeUsuario = prompt("Escolha um nome de Usuário")
-    usuario.nome = nomeUsuario
-    let promisse = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',{
-    name: `${nomeUsuario}` 
-}).then(teste)
+    let nomeUsuario = prompt("Escolha um nome de Usuário");
+    const promisse = axios.post(`https://mock-api.driven.com.br/api/v4/uol/participants`,{
+     name: `${nomeUsuario}` 
+ })
+     promisse.then(response => {
+         logged = true;
+         getParticipants();
+         getMessages();
+     })
+     promisse.catch(error => {
+         alert("Esse nome já está em uso, escolha outro ou aguarde.");
+         pedirNomeUsuario();
+     });
 }
 
-function teste(promisse){
-    console.log(promisse);
+pedirNomeUsuario();
+
+
+//menu nav
+
+function aparecerNav(){
+    const nav = document.querySelector("nav");
+    nav.classList.remove("escondido");
 }
 
-
-
-//renderizar mensagens
-
-let mensagens = [];
-
-function buscarDados(){
-const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants');
-promessa.then(processarResposta);
+function sumirNav(){
+    const sumir = document.querySelector("nav");
+    sumir.classList.add("escondido");
 }
-
-function processarResposta(resposta) {
-	console.log(resposta.data);
-    mensagens = resposta.data;
-    renderizarMensagens();
-}
-
-function renderizarMensagens() {
-    const ulMensagens = document.querySelector(".mensagens");
-    ulMensagens.innerHTML = "";
-    for (let i = 0; i < mensagens.length; i++) {
-      ulReceitas.innerHTML += `
-          <li onClick='renderizarInformações(${i})'>
-          </li>   
-      `;
-    }
-  }
-
-  function renderizarInformações(){
-
-  }
