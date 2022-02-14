e07dc25f9e6d1683710046ef724267fdf5dc7e4e
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-02-14 16:10:11 -0300

    Status, participantes e mensagens prontas

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,97 @@ function pedirNomeUsuario(){
 
 pedirNomeUsuario();
 
+//status
+function checkIfUserOnline(User) {
+    const statusPromisse = axios.post(`https://mock-api.driven.com.br/api/v4/uol/status`, {
+        name: `${nomeUsuario}`
+})
+    statusPromisse.catch(isOffline);
+}
+
+function isOffline(error) {
+    alert("Usuário offline!");
+    pedirNomeUsuario();
+}
+
+setInterval(() => checkIfUserOnline(nomeUsuario), 5000);
+    getMessages();
+    setInterval(getMessages, 3000);
+
+
+//pegar participantes
+function getParticipants() {
+    axios.get(`https://mock-api.driven.com.br/api/v4/uol/participants`)
+        .then(participants)
+}
+
+
+//pegar mensagem
+function getMessages() {
+    const promisseMessage = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promisseMessage.then(getMessagesOK);
+    promisseMessage.catch(didntGetMessages);
+}
+
+function getMessagesOK(response) {
+    const messages = response.data;
+    const containerMessages = document.querySelector(".mensagens");
+    startMessageCount = 0;
+    lastMessageArray = `${messages[messages.length - 1].from}`;
+
+    if (lastMessageArray !== lastMessageLoop || lastMessageLoop === nomeUsuario) {
+        containerMessages.innerHTML = "";
+    while(startMessageCount < messages.length) {
+    const from = messages[startMessageCount].from;
+    const to = messages[startMessageCount].to;
+    const text = messages[startMessageCount].text;
+    const type = messages[startMessageCount].type;
+    const time = messages[startMessageCount].time;
+    if (type === "status") {
+        containerMessages.innerHTML += `
+        <article class="msg-enter-exit ellipsis" data-identifier="message">
+        <p><span>
+                (${time})
+            </span>
+            <strong>${from}</strong> ${text}
+        </p>
+        </article>
+        `;
+    } else if (type === "message") {
+        containerMessages.innerHTML += `
+        <article class="msg-all ellipsis" data-identifier="message">
+        <p><span>
+                (${time})
+            </span>
+            <strong>${from}</strong> para <strong>${to}</strong>: ${text}
+        </p>
+        </article>
+        `;
+    } else if ((type === "private_message" && to === nomeUsuario) || (type === "private_message" && from === nomeUsuario)) {
+        containerMessages.innerHTML += `
+        <article class="msg-pvt ellipsis" data-identifier="message">
+        <p><span>
+                (${time})
+            </span>
+            <strong>${from}</strong> para <strong>${to}</strong>: ${text}
+        </p>
+        </article>
+        `;
+    }
+    startMessageCount++;
+}
+
+const lastElement = document.querySelector("article:last-child");
+lastMessageLoop = lastElement.querySelector("strong").innerText;
+lastElement.scrollIntoView();
+}
+}
+
+function didntGetMessages(error) {
+console.log(error);
+alert("Houve um problema com as mensagens, tente reiniciar");
+}
+
 
 //menu nav
 
